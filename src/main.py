cb50a26be6914cb0dc54c470e1bf4f87d69ef446
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-11-17 18:38:38 -0300

    Modo colador activado

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers.instructores import router as instructores_router
 from controllers.alumnos import router as alumnos_router
 from controllers.clases import router as clases_router
@@ -6,8 +7,16 @@ from controllers.users import router as users_router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
+# Incluyendo los routers
 app.include_router(instructores_router, prefix="/api/instructores", tags=["Instructores"])
 app.include_router(alumnos_router, prefix="/api/alumnos", tags=["Alumnos"])
 app.include_router(clases_router, prefix="/api/clases", tags=["Clases"])
 app.include_router(users_router, prefix="/api/users", tags=["Users"])
-
